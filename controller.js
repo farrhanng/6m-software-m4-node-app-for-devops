f0ab8a277a0f9b191e655dc9dd1259c6ef6cae81
Farrhan <work.farrhan@gmail.com>
2023-10-11 21:42:53 +0800

    successfully deployed

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 
 // This is the only function that will be called by the endpoint.
 function print(req, res){
-    res.send("Hello world!!");
+    res.send("Hello I am Farrhan 👋🏽");
 }
 
 module.exports = print;
